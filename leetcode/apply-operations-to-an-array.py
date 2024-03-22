0b34ab7ef6a58b73af69e510036b0015c5869136
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2024-03-22 03:07:34 +0300

    Create apply-operations-to-an-array.py

---
+++ leetcode/apply-operations-to-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def applyOperations(self, nums: list[int]) -> list[int]:
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i]*=2
+                nums[i+1] = 0
+        i = 0
+        j = 0
+        while i < len(nums):
+            if nums[i]!=0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j+=1
+            i+=1
+        return nums
