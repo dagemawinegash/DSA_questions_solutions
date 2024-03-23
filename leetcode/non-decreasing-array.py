ab5b9cfff6eb89461bebeef09fc90a66ba21d8e4
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2024-03-23 20:30:43 +0300

    Create non-decreasing-array.py

---
+++ leetcode/non-decreasing-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        counter = 0
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                counter += 1
+                if i > 0 and nums[i-1] > nums[i+1]:
+                    nums[i+1] = nums[i]
+                else:
+                    nums[i] = nums[i+1]
+            if counter > 1:
+                return False
+        return True
