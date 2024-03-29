887f935bf135307388ff497c3569f227ef799f1f
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2024-03-29 11:12:11 +0300

    Update merging-arrays.py

--- CodeForces/merging-arrays.py
+++ CodeForces/merging-arrays.py
@@ -10,10 +10,9 @@ c = []
 while p1 < x or p2 < y:
     if p2 == y or (p1 < x and arr1[p1] < arr2[p2]):
         c.append(arr1[p1])
-        k += 1
         p1 += 1
     else:
         c.append(arr2[p2])
-        k += 1
         p2 += 1
+    k += 1
 print(*c)
