8aa26f76377c760e6b882c557df2a032356d3d0b
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2024-03-29 12:39:16 +0300

    Update valid-sudoku.py

--- leetcode/valid-sudoku.py
+++ leetcode/valid-sudoku.py
@@ -2,7 +2,7 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         columns = defaultdict(set)
         rows = defaultdict(set)
-        squares = defaultdict(set)  # key = (r // 3, c // 3)
+        squares = defaultdict(set)
  
         for r in range(9):
             for c in range(9):
