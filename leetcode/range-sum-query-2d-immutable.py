07f9d4359871ccf6657903f714125f5317447781
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2024-04-18 21:17:34 +0300

    Update range-sum-query-2d-immutable.py

--- leetcode/range-sum-query-2d-immutable.py
+++ leetcode/range-sum-query-2d-immutable.py
@@ -11,6 +11,3 @@ class NumMatrix:
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return self.psa[row2+1][col2+1] - self.psa[row1][col2+1] - self.psa[row2+1][col1] + self.psa[row1][col1]
 
-# Your NumMatrix object will be instantiated and called as such:
-# obj = NumMatrix(matrix)
-# param_1 = obj.sumRegion(row1,col1,row2,col2)
