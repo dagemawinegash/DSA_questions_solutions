25a871acf4c81081158720446617fa5e02fb105b
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2024-06-08 01:17:29 +0300

    Update symmetric-tree.py

--- leetcode/symmetric-tree.py
+++ leetcode/symmetric-tree.py
@@ -1,11 +1,12 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
 class Solution:
-    def isSymmetric(self, root) -> bool:
+    def isSymmetric(self, root: TreeNode) -> bool:
         def helper(left, right) -> bool:
             if not left and not right:
                 return True
@@ -19,3 +20,4 @@ class Solution:
             return helper(left.left, right.right) and helper(left.right, right.left)
         
         return helper(root.left, root.right)
+        
