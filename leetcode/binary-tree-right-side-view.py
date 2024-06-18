452b21cd378ed6be8c60fab710d8204e99b9b454
Dagemawi <Dagemawibekele04@gmail.com>
2024-06-19 00:19:23 +0300

    Update binary-tree-right-side-view.py

--- leetcode/binary-tree-right-side-view.py
+++ leetcode/binary-tree-right-side-view.py
@@ -10,13 +10,13 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> list[int]:
         arr = []
         
-        def dfs(node, depth):
+        def helper(node, depth):
             if node:
                 if depth == len(arr): 
                     arr.append(node.val)
                 
-                dfs(node.right, depth + 1)
-                dfs(node.left, depth + 1)
+                helper(node.right, depth + 1)
+                helper(node.left, depth + 1)
         
-        dfs(root, 0)
+        helper(root, 0)
         return arr
