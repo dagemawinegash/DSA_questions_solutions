958ad6c00bce1f36ab04b54ccc4d08d161937237
Dagemawi <Dagemawibekele04@gmail.com>
2024-06-27 16:37:25 +0300

    Create set-mismatch.py

---
+++ leetcode/set-mismatch.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findErrorNums(self, nums: list[int]) -> list[int]:
+        def cycleSort(arr):
+            n = len(arr)
+            i = 0
+            while i < n:
+                correct_position = arr[i] - 1
+                if arr[i] != arr[correct_position]:
+                    arr[correct_position], arr[i] = arr[i], arr[correct_position]
+                else:
+                    i += 1
+            return arr
+        
+        nums = cycleSort(nums)
+        
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return [nums[i], i + 1]
