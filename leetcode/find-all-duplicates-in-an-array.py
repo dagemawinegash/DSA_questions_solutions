176921e122b19aa54928d8a95f43c96698f8505f
Dagemawi <Dagemawibekele04@gmail.com>
2024-06-27 16:32:43 +0300

    Create find-all-duplicates-in-an-array.py

---
+++ leetcode/find-all-duplicates-in-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDuplicates(self, nums: list[int]) -> list[int]:
+        def cycleSort(arr):
+            n = len(arr)
+            i = 0
+            while i < n:
+                correct_position = arr[i] - 1
+                if arr[i] != arr[correct_position]:
+                    arr[correct_position], arr[i] = arr[i], arr[correct_position]
+                else:
+                    i += 1
+            return arr
+        
+        nums = cycleSort(nums)
+        
+        ans = []
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                ans.append(nums[i])
+        
+        return ans
