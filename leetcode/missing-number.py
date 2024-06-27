e7da27541c70037ceeb14fd98b23a0a78a53a260
Dagemawi <Dagemawibekele04@gmail.com>
2024-06-27 15:30:12 +0300

    Update missing-number.py

--- leetcode/missing-number.py
+++ leetcode/missing-number.py
@@ -1,6 +1,20 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2 
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
+        def cycleSort(arr):
+            n = len(arr)
+            i = 0
+            while i < n:
+                correct_position = arr[i]
+                if correct_position < n and arr[correct_position] != arr[i]:
+                    arr[correct_position], arr[i] = arr[i], arr[correct_position]
+                else:
+                    i += 1
+            return arr
+
+        nums = cycleSort(nums)
+
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        
+        return len(nums)
