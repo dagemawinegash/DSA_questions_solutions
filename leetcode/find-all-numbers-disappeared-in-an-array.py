53aa4425fc70ce756c81bbd90e2083612df954e0
Dagemawi <Dagemawibekele04@gmail.com>
2024-06-27 16:56:48 +0300

    Update find-all-numbers-disappeared-in-an-array.py

--- leetcode/find-all-numbers-disappeared-in-an-array.py
+++ leetcode/find-all-numbers-disappeared-in-an-array.py
@@ -1,17 +1,16 @@
 class Solution:
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        def cycleSort(arr):
-            n = len(arr)
+        def cycleSort(nums):
+            n = len(nums)
             i = 0
             while i < n:
-                correct_position = arr[i] - 1
-                if arr[i] != arr[correct_position]:
-                    arr[correct_position], arr[i] = arr[i], arr[correct_position]
+                correct_position = nums[i] - 1
+                if nums[i] != nums[correct_position]:
+                    nums[correct_position], nums[i] = nums[i], nums[correct_position]
                 else:
                     i += 1
-            return arr
         
-        nums = cycleSort(nums)
+        cycleSort(nums)
         
         ans = []
         for i in range(len(nums)):
