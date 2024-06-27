7cf0614c9299036b026bcd0a769e257c55c9ca9a
Dagemawi <Dagemawibekele04@gmail.com>
2024-06-27 17:03:53 +0300

    Update find-the-duplicate-number.py

--- leetcode/find-the-duplicate-number.py
+++ leetcode/find-the-duplicate-number.py
@@ -1,17 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: list[int]) -> int:
-        def cycleSort(arr):
-            n = len(arr)
+        def cycleSort(nums):
+            n = len(nums)
             i = 0
             while i < n:
-                correct_position = arr[i] - 1
-                if arr[i] != arr[correct_position]:
-                    arr[correct_position], arr[i] = arr[i], arr[correct_position]
+                correct_position = nums[i] - 1
+                if nums[i] != nums[correct_position]:
+                    nums[correct_position], nums[i] = nums[i], nums[correct_position]
                 else:
                     i += 1
-            return arr
         
-        nums = cycleSort(nums)
+        cycleSort(nums)
         
         for i in range(len(nums)):
             if nums[i] != i + 1:
