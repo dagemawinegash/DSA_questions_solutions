b6540d75a1df11cadafef9685d2868936d13b6a6
Dagemawi <Dagemawibekele04@gmail.com>
2024-07-01 17:15:04 +0300

    update

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums [i-1]:
                 continue
-            l,r = i+1, len(nums)-1
+            l, r = i + 1, len(nums)-1
             while l < r:
                 threesum = nums[i] + nums [l] + nums [r]
                 if threesum > 0:
