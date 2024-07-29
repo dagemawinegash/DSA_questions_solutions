2bdfab8bf3879a0f2ca935fa28bc3d222293f9bd
Dagemawi <Dagemawibekele04@gmail.com>
2024-07-30 00:12:07 +0300

    Create top-k-frequent-elements.py

---
+++ leetcode/top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        counter = Counter(nums)
+        heap = [(-val, key) for key, val in counter.items()]
+        heapq.heapify(heap)
+
+        ans = [heapq.heappop(heap)[1] for _ in range(k)]
+        
+        return ans
