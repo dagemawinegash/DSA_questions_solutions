5d7988d8982967b0364cb83503c044fca44bb1bc
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2025-03-01 23:13:46 +0300

    Created '4Sum' by A2SV Hub

---
+++ 01-Mar-2025/4Sum 287858.py
@@ -0,0 +1,27 @@
+# Problem: 4Sum - https://leetcode.com/problems/4sum/
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        arr = []
+        for i in range (len(nums)):
+            if i > 0 and  nums [i] == nums [i-1]:
+                continue
+            for j in range (i+1, len(nums)):
+                if j > i+1 and nums[j] == nums [j-1]:
+                    continue
+                
+                l,r = j+1, len(nums)-1
+                while l < r:
+                    foursum = nums[i] + nums [j] + nums [l] + nums [r]
+                    if foursum > target:
+                        r-=1
+                    elif foursum < target:
+                        l+=1
+                    else:
+                        arr.append([nums[i], nums [j], nums [l], nums [r]])
+                        l+=1
+                        while  l < r and nums [l] == nums [l-1]: 
+                            l+=1               
+        return arr
+            
