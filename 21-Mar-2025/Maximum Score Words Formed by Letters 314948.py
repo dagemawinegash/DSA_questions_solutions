208de3ac5ab644857cdc239619ea7b673c6955ee
dagemawinegash <162117348+dagemawinegash@users.noreply.github.com>
2025-03-21 19:34:24 +0300

    Created 'Maximum Score Words Formed by Letters' by A2SV Hub

---
+++ 21-Mar-2025/Maximum Score Words Formed by Letters 314948.py
@@ -0,0 +1,37 @@
+# Problem: Maximum Score Words Formed by Letters - https://leetcode.com/problems/maximum-score-words-formed-by-letters/description/
+
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        n = len(words)
+        d = Counter(letters)
+        def calculate(count):
+            res = 0
+            for char in count:
+                res += score[ord(char) - ord("a")] * count[char]
+            return res
+
+        def helper(i, arr):
+            nonlocal ans
+            if i == n:
+                count = Counter("".join(arr))
+                if count <= d:
+                    ans = max(ans, calculate(count))
+                return
+            arr.append(words[i])
+            helper(i + 1, arr)
+            arr.pop()
+            helper(i + 1, arr)
+
+        ans = 0
+        helper(0, [])
+        return ans
+                
+
+        
+        
+        
+
+
+
+
+        
